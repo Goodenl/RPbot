a4db5c27ca700614a684c3c6c98807677f79c729
Goodenl <denmatyash522@gmail.com>
2020-08-01 02:10:39 +0300

    Ehhh...

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
+import json
 import os
-import re
 import telebot
+# import mega_upload as mu
 
 # Изменить!
-api_token = "1331026510:AAFDJtzfgSsKnM43R1ilvokVgrg8oAkThHI"
-api = os.getenv("RP_API_TOKEN")
+api_token = os.getenv("RP_API_TOKEN")
 
 path_to_write = os.getenv("RP_PATH")
-episod_number = "Эпизод 1"
+episod_number = "Episod 1"
 
 bot = telebot.TeleBot(api_token)
 
 #save all message in file
 def save_message(text):
-	with open(f"{path_to_write}{episod_number}\\temp.txt", "a") as w:
-		w.write(f"{text}\n")
-		w.close()
+	if not(text.startswith("\\")):
+
+		with open(f"{path_to_write}{episod_number}\\temp.txt", "a", encoding="utf-8") as w:
+			w.write(f"{text}\n")
+			w.close()
 
 @bot.message_handler(content_types=["text"])
-def send_var(message):
-	bot.send_message(message.chat.id, message)
+def get_message(message):
+	bot.send_message(message.chat.id, message.text)
 
 	save_message(message.text)
+
+	# mu.upload_on_cloud()
 
 
 bot.polling(none_stop=True)
