1e4a201cba0da79621b66c77c21620aad5ba8329
Goodenl <denmatyash522@gmail.com>
2020-08-01 23:42:23 +0300

    Add sync with cloud

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import json
 import os
 import telebot
-# import mega_upload as mu
+import mega_upload as mu
 
-# Изменить!
+
 api_token = os.getenv("RP_API_TOKEN")
 
 path_to_write = os.getenv("RP_PATH")
@@ -11,21 +10,31 @@ episod_number = "Episod 1"
 
 bot = telebot.TeleBot(api_token)
 
+counter_package = 0
+
 #save all message in file
-def save_message(text):
+def save_message(text, counter):
+	
 	if not(text.startswith("\\")):
 
 		with open(f"{path_to_write}{episod_number}\\temp.txt", "a", encoding="utf-8") as w:
 			w.write(f"{text}\n")
 			w.close()
 
+	#upload on server
+	counter += 1
+	if counter == 5:
+		mu.upload_on_cloud() # Как оно работает??
+		counter = 0
+
 @bot.message_handler(content_types=["text"])
 def get_message(message):
-	bot.send_message(message.chat.id, message.text)
+	if message.chat.type == "private":
+		bot.send_message(message.chat.id, message.text)
 
-	save_message(message.text)
-
-	# mu.upload_on_cloud()
+		save_message(message.text, counter_package)
+	else:
+		save_message(message.text, counter_package)
 
 
 bot.polling(none_stop=True)
--- mega_upload.py
+++ mega_upload.py
@@ -4,6 +4,7 @@ from mega import Mega
 
 email = os.getenv("Mega_EMAIL")
 passw = os.getenv("Mega_PASSWORD")
+
 #get files position
 path_to_write = os.getenv("RP_PATH")
 episod_number = "Episod 1"
@@ -11,18 +12,10 @@ episod_number = "Episod 1"
 mega = Mega()
 m = mega.login(email, passw)
 
-
 # find folder and upload file
 def upload_on_cloud():
 	folder = m.find('Episod 1')
 
 	file = m.upload(f"{path_to_write}{episod_number}\\temp.txt", folder[0])
 
-	#delet prevers update
-	f = m.find(filename="temp.txt")
-	last_update = m.get_link(f)
-	try:
-		print(last_update)
-		m.delete_url(last_update)
-	except:
-		pass
+	print(file)
